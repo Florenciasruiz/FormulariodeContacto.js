407a1f07d38a9226a09d05f8c7f8ac517df41c58
FlorenciaRuiz <florenciasruiz95@gmail.com>
2023-11-23 01:26:27 -0300

    Segunda preentrega modificado

--- js/main.js
+++ js/main.js
@@ -1,46 +1,34 @@
-// Objeto para almacenar la información del usuario
-let usuario = {
-  nombre: '',
-  email: '',
-  mensaje: ''
-};
+// Array para almacenar la información del usuario
+let usuario = [];
 
 // Función para solicitar información al usuario
 function solicitarInformacion(dato, mensaje) {
   let input = prompt(mensaje);
-  if (input === null) {
-    alert("Has cancelado el proceso.");
+  if (input === null || input === '') {
+    alert("Has cancelado el proceso o el campo está vacío.");
     return null;
-  }
-  while (!input) {
-    input = prompt(dato + " es obligatorio. Ingrese su " + dato.toLowerCase() + ":");
-    if (input === null) {
-      alert("Has cancelado el proceso.");
-      return null;
-    }
   }
   return input;
 }
 
-// Función para confirmar la información proporcionada por el usuario
-function confirmarInformacion(usuario) {
-  alert("¡Gracias por completar el formulario! Nombre: " + usuario.nombre + ", Email: " + usuario.email + ", Mensaje: " + usuario.mensaje);
-}
-
 // Función principal para solicitar la información
 function solicitarDatosUsuario() {
-  usuario.nombre = solicitarInformacion("Nombre", "Por favor, ingrese su nombre:");
-  if (usuario.nombre !== null) {
-    usuario.email = solicitarInformacion("Email", "Por favor, ingrese su dirección de correo electrónico:");
-    if (usuario.email !== null) {
-      usuario.mensaje = solicitarInformacion("Mensaje", "Por favor, ingrese su mensaje:");
-      if (usuario.mensaje !== null) {
-        confirmarInformacion(usuario);
-      }
+  const datos = ["Nombre", "Email", "Mensaje"];
+
+  for (const dato of datos) {
+    let info = solicitarInformacion(dato, `Por favor, ingrese su ${dato}:`);
+    if (info !== null) {
+      usuario.push({ [dato]: info });
+    } else {
+      usuario.length = 0; 
+      return;
     }
+  }
+
+  if (usuario.length === datos.length) {
+    alert("¡Gracias por completar el formulario!");
+    console.log(usuario);
   }
 }
 
-// Llamada a la función principal
 solicitarDatosUsuario();
-
