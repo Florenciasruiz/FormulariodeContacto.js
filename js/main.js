396f30342b5c933ad306b1e1cbc8544d78f6cb7a
FlorenciaRuiz <florenciasruiz95@gmail.com>
2023-12-06 16:42:59 -0300

    Tercer preEntrega Actualiz

--- js/main.js
+++ js/main.js
@@ -1,34 +1,42 @@
-// Array para almacenar la información del usuario
-let usuario = [];
 
-// Función para solicitar información al usuario
-function solicitarInformacion(dato, mensaje) {
-  let input = prompt(mensaje);
-  if (input === null || input === '') {
-    alert("Has cancelado el proceso o el campo está vacío.");
-    return null;
-  }
-  return input;
+// Datos simulados de tareas
+const tasks = [
+  { id: 1, title: 'Tarea 1', completed: false },
+  { id: 2, title: 'Tarea 2', completed: true },
+  { id: 3, title: 'Tarea 3', completed: false }
+];
+
+// Métodos de Arrays
+const incompleteTasks = tasks.filter(task => !task.completed);
+console.log("Tareas incompletas:", incompleteTasks);
+
+// Funciones y condicionales
+function mostrarTareasIncompletas() {
+  const incompleteTasksList = document.getElementById('incompleteTasks');
+  incompleteTasksList.innerHTML = ''; // Limpiar el contenido anterior
+
+  incompleteTasks.forEach(task => {
+    const taskElement = document.createElement('li');
+    taskElement.textContent = task.title;
+    incompleteTasksList.appendChild(taskElement);
+  });
 }
 
-// Función principal para solicitar la información
-function solicitarDatosUsuario() {
-  const datos = ["Nombre", "Email", "Mensaje"];
+//  DOM y eventos
+document.addEventListener('DOMContentLoaded', () => {
+  mostrarTareasIncompletas();
+});
 
-  for (const dato of datos) {
-    let info = solicitarInformacion(dato, `Por favor, ingrese su ${dato}:`);
-    if (info !== null) {
-      usuario.push({ [dato]: info });
-    } else {
-      usuario.length = 0; 
-      return;
-    }
-  }
+// Objeto para manejar el formulario de contacto
+const contactForm = document.getElementById('contactForm');
+contactForm.addEventListener('submit', (event) => {
+  event.preventDefault();
 
-  if (usuario.length === datos.length) {
-    alert("¡Gracias por completar el formulario!");
-    console.log(usuario);
-  }
-}
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
 
-solicitarDatosUsuario();
+  const formData = { name, email, message };
+
+  console.log("Datos del formulario:", formData);
+});
